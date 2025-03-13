e9b95adfece3f611553b1369f500699f2c44faad
Eetu Turtiainen <deetu001@edu.xamk.fi>
2025-03-13 20:31:08 +0200

    Juupelis juu

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,11 @@
-
-return  {
-
-          'sainnhe/gruvbox-material',
-          lazy = false,
-          priority = 1000,
-          config = function()
-            -- Optionally configure and load the colorscheme
-            -- directly inside the plugin declaration.
-            -- vim.g.gruvbox_material_transparent_background = 1
-            vim.g.gruvbox_material_enable_italic = true
-            vim.cmd.colorscheme('gruvbox-material')
-          end
+return {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("cyberdream").setup({
+            transparent = true,
+        });
+        vim.cmd("colorscheme cyberdream")
+    end,
 }
